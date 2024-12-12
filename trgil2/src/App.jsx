3da83c356d4d4aad65f0412c7dc99944fbe5f376
naorkeren <123155936+naorkeren@users.noreply.github.com>
2024-12-12 13:44:19 +0200

    tragil

---
+++ trgil2/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import ClassT from './ClassT/ClassT'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <ClassT>
+      
+    </ClassT>
+    </>
+  )
+}
+
+export default App
---
+++ trgil2/src/ClassT/ClassT.jsx
@@ -0,0 +1,25 @@
+import { Component } from "react";
+
+export default class ClassT extends Component{
+    constructor(props)
+    {
+        super(props)
+
+    }
+    render(){
+        return(
+          <div>
+              <div style = {{ border: '5px solid blue',
+                margin:5,padding:5,borderRadius:15 }}>
+                   num1 :  
+                   <input onCange ={} />
+           </div>
+           <div style = {{ border: '5px solid blue',
+                margin:5,padding:5,borderRadius:15 }}>
+                   num1 :
+
+           </div>
+          </div>
+        )
+    }
+}
